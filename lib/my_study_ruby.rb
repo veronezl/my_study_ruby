574fd4a7fd14b452a5821c5c37d693f4e95ff432
veronezl <lucasveronez.dev@gmail.com>
2024-07-17 09:40:01 -0300

    Initial commit

---
+++ lib/my_study_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "my_study_ruby/version"
+
+module MyStudyRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/my_study_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MyStudyRuby
+  VERSION = "0.1.0"
+end
