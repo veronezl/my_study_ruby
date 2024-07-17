3ce59aabf90e95c345406385adac73dbb95c0f7a
veronezl <lucasveronez.dev@gmail.com>
2024-07-17 13:56:15 -0300

    Second commit

--- spec/my_study_ruby_spec.rb
+++ spec/my_study_ruby_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe MyStudyRuby do
   it "has a version number" do
     expect(MyStudyRuby::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
